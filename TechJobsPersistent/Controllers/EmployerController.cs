4e8e42eebbe0f55db17885f8164d11caae385942
Amy Gamble <amymgambl@gmail.com>
2020-12-11 10:45:43 -0600

    added new view model and updated some views for adding employer

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,25 +15,57 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+        //DB variable step 2.2.1
+        private JobDbContext context;
+        public EmployerController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
         // GET: /<controller>/
+        // Added code for 2.2.2
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employers = context.Employers.ToList();
+
+            return View(employers);
         }
 
+        // Step 2.2.3
         public IActionResult Add()
         {
-            return View();
+            AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
+            return View(addEmployerViewModel);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        // Step 2.2.4
+        [HttpPost]
+        public IActionResult ProcessAddEmployerForm(AddEmployerViewModel viewModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                Employer newEmployer = new Employer
+                {
+                    Name = viewModel.Name,
+                    Location = viewModel.Location
+                };
+
+                context.Employers.Add(newEmployer);
+                context.SaveChanges();
+
+                return Redirect("/Employer");
+            }
+            return View(viewModel);
         }
 
+        // Step 2.2.5
         public IActionResult About(int id)
         {
-            return View();
+            Employer theEmployer = context.Employers
+                .Where(emp => emp.Id == id);
+
+            AddEmployerViewModel viewModel = new AddEmployerViewModel(theEmployer);
+
+            return View(theEmployer);
         }
     }
 }
